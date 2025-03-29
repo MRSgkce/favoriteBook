a4d734662f967187bd64c0b918917f5f1dfb602e
MRSgkce <mursidegokce985@gmail.com>
2025-03-29 12:49:11 +0300

    Initial Commit

---
+++ favoriteBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  favoriteBook
+//
+//  Created by Mürşide Gökçe on 29.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ favoriteBook/favoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  favoriteBookApp.swift
+//  favoriteBook
+//
+//  Created by Mürşide Gökçe on 29.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct favoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
